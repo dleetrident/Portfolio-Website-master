572ca51c595111a22458ecb0b85b60a615c9c70e
Daniel Lee <dleetrident@hotmail.com>
2021-06-29 11:11:28 +0100

    small tweaks

---
+++ Portfolio-Website-master/index.js
@@ -0,0 +1,51 @@
+  $('.scrollnav').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() < 400) {
+    $('.scrollnav').fadeOut()
+  } else {
+    $('.scrollnav').fadeIn();
+  }
+});
+
+// Slide Up Attempt
+
+(function($) {
+ $.fn.visible = function(partial) {
+
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+
+})(jQuery);
+
+var win = $(window);
+
+var allMods = $(".module");
+
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible");
+  }
+});
+
+win.scroll(function(event) {
+
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in");
+    }
+  });
+
+});
